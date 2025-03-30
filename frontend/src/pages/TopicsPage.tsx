7158ba9c6a56bf998208737e378f9a7baa33af30
avinash6 <avinash.sg7@gmail.com>
2025-03-30 23:11:10 +0800

    feat: allow topics to be displayed primitively

--- frontend/src/pages/TopicsPage.tsx
+++ frontend/src/pages/TopicsPage.tsx
@@ -1,7 +1,30 @@
+import {useEffect, useState} from "react";
+import {useApiClientContext} from "../provider/ApiClientProvider.tsx";
+
 function TopicsPage() {
+    const [topics, setTopics] = useState<string[]>([])
+    const [error, setError] = useState<string | null>(null)
+    const apiClient = useApiClientContext()
+
+    useEffect(() => {
+        apiClient.topicsGet().then(
+            (response) => {
+                setTopics(response)
+            }
+        ).catch(
+            (error) => {
+                setError(error.message)
+            }
+        )
+    }, [apiClient])
+
     return (
         <div>
             <h1>Topics</h1>
+            {error && <p>{error}</p>}
+            <ul>
+                {topics.map((topic) => <li key={topic}>{topic}</li>)}
+            </ul>
         </div>
     )
 }
