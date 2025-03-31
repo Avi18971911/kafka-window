614a1cba8f576c9b26611d736cf2c5237433e1e2
avinash6 <avinash.sg7@gmail.com>
2025-03-31 21:52:08 +0800

    feat: add additional information to all topics query

--- backend/internal/kafka/kafka_service.go
+++ backend/internal/kafka/kafka_service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Avi18971911/kafka-window/backend/internal/kafka/model"
 	"github.com/IBM/sarama"
 	"go.uber.org/zap"
+	"strconv"
 )
 
 type KafkaService struct {
@@ -40,17 +41,15 @@ func (k *KafkaService) ConnectToCluster(brokers []string, config *sarama.Config)
 	return nil
 }
 
-func (k *KafkaService) GetTopics() ([]string, error) {
+func (k *KafkaService) GetTopics() ([]model.TopicDetails, error) {
 	topicMap, err := k.admin.ListTopics()
 	if err != nil {
 		k.logger.Error("KafkaService can't get topics: failed to get topics", zap.Error(err))
 		return nil, err
 	}
-	topics := make([]string, 0, len(topicMap))
-	for topic, _ := range topicMap {
-		topics = append(topics, topic)
-	}
-	return topics, nil
+
+	topicDetails := k.getTopicDetailsFromTopicMap(topicMap)
+	return topicDetails, nil
 }
 
 func (k *KafkaService) GetConsumerGroups() ([]string, error) {
@@ -267,6 +266,103 @@ func (k *KafkaService) getConsumerToTopicToPartitionOffsetsMap(
 		}
 	}
 	return consumerToTopicToPartitionOffsetMap, nil
+}
+
+func (k *KafkaService) getTopicDetailsFromTopicMap(
+	topicMap map[string]sarama.TopicDetail,
+) []model.TopicDetails {
+	topicDetails := make([]model.TopicDetails, len(topicMap))
+	i := 0
+	for topic, topicDetail := range topicMap {
+		additionalConfigs := make(map[string]string)
+		var cleanupPolicy = model.CleanupPolicyUnknown
+		var retentionMs int64 = -1
+		var retentionBytes int64 = -1
+		for configKey, config := range topicDetail.ConfigEntries {
+			if config == nil {
+				k.logger.Warn(
+					"nil config encountering during getTopicDetailsFromTopicMap. Skipping...",
+					zap.String("topic", topic),
+				)
+				continue
+			}
+			switch configKey {
+			case "cleanup.policy":
+				cleanupPolicy = getCleanupPolicy(config)
+				if cleanupPolicy == model.CleanupPolicyUnknown {
+					k.logger.Warn(
+						"encountered unknown cleanup policy",
+						zap.String("topic", topic),
+						zap.String("policy", *config),
+					)
+				}
+			case "retention.ms":
+				rMs, err := strconv.ParseInt(*config, 10, 64)
+				if err != nil {
+					k.logger.Error(
+						"error converting retention.ms to number",
+						zap.String("topic", topic),
+						zap.Error(err),
+					)
+					continue
+				}
+				retentionMs = rMs
+			case "retention.bytes":
+				rBs, err := strconv.ParseInt(*config, 10, 64)
+				if err != nil {
+					k.logger.Error(
+						"error converting retention.bytes to number",
+						zap.String("topic", topic),
+						zap.Error(err),
+					)
+					continue
+				}
+				retentionBytes = rBs
+			default:
+				additionalConfigs[configKey] = *config
+			}
+		}
+		isInternal := isInternalTopic(topic)
+		topicDetails[i] = model.TopicDetails{
+			Name:              topic,
+			NumPartitions:     topicDetail.NumPartitions,
+			ReplicationFactor: topicDetail.ReplicationFactor,
+			IsInternal:        isInternal,
+			CleanupPolicy:     cleanupPolicy,
+			RetentionMs: model.RetentionMs{
+				Indefinite: retentionMs == -1,
+				Value:      retentionMs,
+			},
+			RetentionBytes:    retentionBytes,
+			AdditionalConfigs: additionalConfigs,
+		}
+		i += 1
+	}
+	return topicDetails
+}
+
+func getCleanupPolicy(config *string) model.CleanupPolicy {
+	var cleanupPolicy = model.CleanupPolicyUnknown
+	if config == nil {
+		cleanupPolicy = model.CleanupPolicyUnknown
+	} else {
+		policy := *config
+		switch policy {
+		case "delete":
+			cleanupPolicy = model.CleanupPolicyDelete
+		case "compact":
+			cleanupPolicy = model.CleanupPolicyCompact
+		case "delete,compact", "compact,delete":
+			cleanupPolicy = model.CleanupPolicyBoth
+		default:
+			cleanupPolicy = model.CleanupPolicyUnknown
+		}
+	}
+	return cleanupPolicy
+}
+
+func isInternalTopic(topic string) bool {
+	return len(topic) > 2 && topic[:2] == "__"
 }
 
 func getTopicToPartitionListFromMap(topicPartitionMap map[string]map[int32]bool) map[string][]int32 {
---
+++ backend/internal/kafka/model/TopicDetails.go
@@ -0,0 +1,26 @@
+package model
+
+type TopicDetails struct {
+	Name              string            `json:"name"`
+	NumPartitions     int32             `json:"numPartitions"`
+	ReplicationFactor int16             `json:"replicationFactor"`
+	IsInternal        bool              `json:"isInternal"`
+	CleanupPolicy     CleanupPolicy     `json:"cleanupPolicy"`
+	RetentionMs       RetentionMs       `json:"retentionMs"`
+	RetentionBytes    int64             `json:"retentionBytes"`
+	AdditionalConfigs map[string]string `json:"additionalConfigs"`
+}
+
+type CleanupPolicy string
+
+const (
+	CleanupPolicyDelete  CleanupPolicy = "delete"
+	CleanupPolicyCompact CleanupPolicy = "compact"
+	CleanupPolicyBoth    CleanupPolicy = "both"
+	CleanupPolicyUnknown CleanupPolicy = "unknown"
+)
+
+type RetentionMs struct {
+	Indefinite bool  `json:"indefinite"`
+	Value      int64 `json:"value"`
+}
